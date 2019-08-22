180ae7d15da8cd25404332a5399ee239f381544e
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 12:24:43 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Alumno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public string Peso { get; set; }
+        public string Actor { get; set; }
+
+        public Alumno (string nombre, string matricula, string peso, string actor)
+        {
+            Nombre = nombre;
+            Matricula = matricula;
+            Peso = peso;
+            Actor = actor;
+        }
+        public Alumno(string nombre, string matricula)
+        {
+            Nombre = nombre;
+            Matricula = matricula;
+        }
+
+    }
+}
---
+++ Colecciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+
+            alumnos.Add(new Alumno("Jesus", "189289"));
+            alumnos.Add(new Alumno("Ana", "189100"));
+
+            foreach(Alumno alumno in alumnos)
+            {
+                Console.WriteLine("Nombre: " + alumno.Nombre);
+                Console.WriteLine("Matrícula: " + alumno.Matricula);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
